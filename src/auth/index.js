72a489382c3fb944f4b63f260f290eebfcba8a05
reden1k <dim23062006@gmail.com>
2024-04-26 22:42:25 +0300

    tied up with local server

--- src/auth/index.js
+++ src/auth/index.js
@@ -1,25 +1,15 @@
-// import { Authorization } from '../backend/Authorization.js';
-// import { ipcRenderer, shell } from 'electron';
+import { Authorization } from '../backend/Authorization.js';
 
-// window.addEventListener('DOMContentLoaded', () => {
-//   const authButton = document.querySelector('.auth-button');
-//     authButton.addEventListener('click', () => {
-//       const authButton = document.querySelector('.auth-button');
-//       authButton.classList.toggle('active');
-//       const div = document.querySelector('.auth');
-//       div.classList.toggle('active');
-//       const img = document.querySelector('.icon');
-//       img.classList.toggle('upper')
-//     })
-//   });
-  //   const authButton = document.querySelector('.auth-button');
-  //   authButton.addEventListener('click', (e) => {
-  //     console.log(Authorization.getAuthorizationCode())
-  //     ipcRenderer.send('open-auth-window', (e, Authorization.getAuthorizationCode()));
-  // })
-
-  function toggleAnimation() {
-    var button = document.querySelector('.button-auth');
-    button.classList.toggle('animation-active');
-  }
+window.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.button-auth');
+  button.addEventListener('click', () => {
+    //mb tyt classtoggle
+    const link = Authorization.getLink() // Получить ссылку и вывести ее отсюда
+    const code = Authorization.getAuthCode(link)
+    const tokens = Authorization.requestAccessAndRefreshTokens(code);
+    console.log(link);
+    console.log(code)
+    console.log(tokens)
+  })
+})
   
